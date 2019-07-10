42df30006293c5ca14bb1fed6d400754f50e7e95
mnezmah <mario.nezmah@gmail.com>
2019-07-10 18:19:09 +0200

    Force true deleted

--- db.js
+++ db.js
@@ -5,9 +5,8 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@loca
 const sequelize = new Sequelize(databaseUrl)
 
 sequelize
-  .sync({force: true})
+  .sync()
   .then(() => console.log('db schema updated'))
   .catch(() => console.error)
-
 
 module.exports = sequelize
